ade63bd9e681b6fe25ddc0d062270d04ac3cf075
Mayank Sharma <manku2406raksh@gmail.com>
2026-06-09 22:55:56 +0530

    Modified Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,17 @@
 function Navbar() {
-  return <h1>Navbar Section</h1>;
+  return (
+    <nav>
+      <div>Career Pivot</div>
+
+      <div>
+        <a href="#">How It Works</a>
+        <a href="#">Explore Roles</a>
+        <a href="#">Pricing</a>
+      </div>
+
+      <button>Get Started</button>
+    </nav>
+  );
 }
 
 export default Navbar;
