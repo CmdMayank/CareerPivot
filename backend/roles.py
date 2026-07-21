cd072d8943043e3c54806d0f458c1de2034076da
Mayank Sharma <manku2406raksh@gmail.com>
2026-07-21 23:40:45 +0530

    roles final

--- backend/roles.py
+++ backend/roles.py
@@ -45,7 +45,14 @@ role_requirements = {
         "NumPy"
     ],
 
-    
+    "Data Scientist": [
+        "Python",
+        "Pandas",
+        "NumPy",
+        "SQL",
+        "Machine Learning",
+        "Matplotlib"
+    ],
 
     "Android Developer": [
         "Java",
